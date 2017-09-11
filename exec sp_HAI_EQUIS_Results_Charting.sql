57b7309986c232591e663d430dab4870533cabcf
HaleyAndAldrich <dhiggins@haleyaldrich.com>
2017-09-11 08:29:14 -0700

    filter SLs on Method Group
    So target units are limited to selected params. eg pci/l

--- exec sp_HAI_EQUIS_Results_Charting.sql
+++ exec sp_HAI_EQUIS_Results_Charting.sql
@@ -1,23 +1,24 @@
 use equis
 go
-declare @facility_id int = (select facility_id from dt_facility where facility_name like 'stuart%')
+declare @facility_id int = (select facility_id from dt_facility where facility_name like 'j.m. stuart%')
 
 exec [HAI].[sp_HAI_EQUIS_Results_Charting]
-	 4631670,
-	 null, --@subfacilities varchar(2000)
+	 @facility_id,
+	 'jmss-landfill 9', --@subfacilities varchar(2000)
 	 null, --@location_groups varchar (2000),
 	 null, --'MW-12|MW-10|MW-11|MW-3B', --@locations varchar (2000),
 	 'n', --@sample_type varchar (20),
-	 null, --'1999-Aug-GW|2000-Mar-GW', --@task_codes varchar (1000),
+	 '2017-07-wg-jmss', --'1999-Aug-GW|2000-Mar-GW', --@task_codes varchar (1000),
 	 '1/1/1998', --@start_date datetime, --= 'jan 01 1900 12:00 AM',
-	 '7/1/2017', --@end_date datetime,  -- ='dec 31 2050 11:59 PM',
-	 'p&k gw target compounds', --@analyte_groups varchar(2000),
+	 '7/12/2017', --@end_date datetime,  -- ='dec 31 2050 11:59 PM',
+	 'dpl_stu_app4_rad', --@analyte_groups varchar(2000),
 	 null, --@param varchar (2000),
 	 null, --@fraction varchar(20),
 	 'wg', --@matrix_codes varchar (500),
-	 'ug/l', --@target_unit varchar(100),
-	 null, --@limit_type varchar (10) = 'RL',
+	 'pci/l', --@target_unit varchar(100),
+	 'result', --@limit_type varchar (10) = 'RL',
 	 null, --@user_qual_def varchar (10) = '# Q',
 	 'n', --@show_val_yn varchar (10) = 'N',
 	 'n', --@detected_chem_only varchar (10) = 'N'
-	 null  --@action levels
+	  'fed_Rsl-2017-06_MCL-wg',  --@action levels
+	  'n'
